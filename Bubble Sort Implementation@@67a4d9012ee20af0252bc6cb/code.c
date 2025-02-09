79c84cdca549a4d4f676c5d9f22d8def66c2216d
DivyamSinghania <divyamsinghania65@gmail.com>
2025-02-09 16:58:52 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int bubbleSort(int arr[],int n){
+char* bubbleSort(int arr[],int n){
     for (int i=0;i<(n-1);i++) {
         for (int j=0;j<(n-i-1);j++) {
             if(arr[j]>arr[j+1]){
@@ -10,12 +10,10 @@ int bubbleSort(int arr[],int n){
             }
         }
     }
-    return 0;
 }
 
-int printArray(int arr[],int n){
+char* printArray(int arr[],int n){
     for(int i=0;i<n;i++){printf("%d ",arr[i]);}
-    return 0;
 }
 
 int main() {
