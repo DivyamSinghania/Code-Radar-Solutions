b59ddee2b811594155b82de6ae524e0e9ed1cd46
DivyamSinghania <divyamsinghania65@gmail.com>
2025-03-28 17:17:07 +0530

    Solution to Find the Second Largest Element in an Array

--- Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
+++ Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//     if(n<=1){
+//         printf("-1");
+//     }
+//     else{
+//         int max=arr[0];
+//         int sec=arr[1];
+//         for(int i=1;i<n;i++){
+//             if(max<arr[i]){
+//                 sec=max;
+//                 max=arr[i];
+//             }
+//         }
+//         if(sec==max){printf("-1");}
+//         else
+//             printf("%d",sec);
+//     }
+// }
+
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(){
     int n;
     scanf("%d",&n);
@@ -12,14 +40,17 @@ int main(){
         printf("-1");
     }
     else{
-        int max=arr[0];
-        int sec=arr[1];
-        for(int i=1;i<n;i++){
-            if(max<arr[i]){
-                sec=max;
-                max=arr[i];
+        for(int i=1;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]>arr[j]){
+                    arr[i]=arr[i]^arr[j];
+                    arr[j]=arr[i]^arr[j];
+                    arr[i]=arr[i]^arr[j];
+                }
             }
         }
+        int max=arr[n-1];
+        int sec=arr[n-2];
         if(sec==max){printf("-1");}
         else
             printf("%d",sec);
